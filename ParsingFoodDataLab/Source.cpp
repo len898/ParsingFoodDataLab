5489f932e355d1388313c3acbc57ed7b68d5d91d
lennart@familie-richter.de <lennart@familie-richter.de>
2023-03-08 09:33:52 -0800

    Finished using online editor, need to edit this later

--- ParsingFoodDataLab/Source.cpp
+++ ParsingFoodDataLab/Source.cpp
@@ -5,8 +5,11 @@
 void parseData(std::string inputString);
 
 int main() {
+	std::string textFile;
 	std::ifstream inPutStream;
-	inPutStream.open("food.txt");
+
+	std::cin >> textFile;
+	inPutStream.open(textFile);
 
 	if (inPutStream.is_open()) {
 		while (inPutStream.good()) {
@@ -22,8 +25,26 @@ int main() {
 }
 
 void parseData(std::string inputString) {
-	if (inputString.substr(inputString.length() - 9, 9) == "Available") {
-		std::cout << inputString << std::endl;
+	//std::cout << stringCopy << std::endl;
+	 //if (stringCopy.find("not available") == std::string::npos && stringCopy.find("available") != std::string::npos) {
+	std::size_t foundFirst = inputString.find('\t');
+	std::string category = inputString.substr(0, foundFirst);
+	//std::cout << category << std::endl;
+
+	std::size_t foundSecond = inputString.find('\t', foundFirst + 1);
+	std::string name = inputString.substr(foundFirst + 1, foundSecond - foundFirst - 1);
+	//std::cout << name << std::endl;
+
+	std::size_t foundThird = inputString.find('\t', foundSecond + 1);
+	std::string description = inputString.substr(foundSecond + 1, foundThird - foundSecond - 1);
+
+	std::size_t foundFourth = inputString.find('\t', foundThird + 1);
+	std::string availability = inputString.substr(foundThird + 1, foundFourth - foundThird - 1);
+
+	if (availability == "Available") {
+		std::cout << name << " (" << category << ") -- " << description << std::endl;
 	}
+
+	//}
 
 }
